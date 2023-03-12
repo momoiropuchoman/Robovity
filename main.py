a01ae8b6c68741fcc67ec57edcc316e4fb751cb1
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-03-12 15:01:43 -0700

    Comment what ChatGPT replies

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ class App:
 
         self.conversation = []
 
+        self.translator = Translator(from_lang = "ja", to_lang = "zh-cn")
+
     def run(self):
 
         with ThreadPoolExecutor(max_workers=2) as executor:
@@ -49,13 +51,26 @@ class App:
                 # recognize speech using Google Speech Recognition
                 try:
                     text = self.r.recognize_google(audio, language='ja-JP')
+                    #text = self.r.recognize_google(audio, language='cmn-Hans-CN')
+                    #text = self.r.recognize_google(audio, language='en-US')
                 except sr.UnknownValueError:
                     pass
                 except sr.RequestError as e:
                     pass
                 else:
                     print(text)
-                    self.comment(text)
+                    print("")
+                    #text = self.get_reply_from_chatpgt("この次の文章に対して130字以内で会話のように返答してください。" + text)
+                    text = self.get_reply_from_chatpgt("この次の文章に対して簡単に返答してください。" + text)
+                    #text = self.translator.translate(text)
+                    #print(text)
+
+                    length = len(text)
+                    split_text = [text[i:i+130] for i in range(0, length, 130)]
+
+                    for t in split_text:
+                        self.comment(t)
+                        time.sleep(1)
             
             time.sleep(0.3)
 
@@ -99,13 +114,17 @@ class App:
 
             time.sleep(7)
     
-    def get_reply_from_chatpgt(question):
+    def get_reply_from_chatpgt(self, question):
+        print("before gpt")
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
             {"role": "user", "content": question},
         ],)
         str = response.choices[0]["message"]["content"].strip()
+        print("get ans")
+
+        return str
         
     def move_to_send(self):
         pyautogui.move(MOVE_X, 0)
